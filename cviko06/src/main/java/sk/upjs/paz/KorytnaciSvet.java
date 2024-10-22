a4445e6628d7d9a69c73a447063b8885ce8a6825
mikulasmarko <5410498@upjs.sk>
2024-10-22 16:22:32 +0200

    cviko 6 start

---
+++ cviko06/src/main/java/sk/upjs/paz/KorytnaciSvet.java
@@ -0,0 +1,39 @@
+import java.awt.event.MouseEvent;
+
+import sk.upjs.jpaz2.*;
+
+public class KorytnaciSvet extends WinPane {
+    /**
+     * Referencia na pole korytnaciek
+     */
+    private Turtle[] korytnacky = null;
+
+    /**
+     * Inicializacna metoda (konstruktor)
+     */
+    public KorytnaciSvet() {
+        this.korytnacky = new Turtle[0];
+    }
+
+    /**
+     * Metoda na pridanie korytnacky na zadanych suradniciach
+     */
+    public void pridajKorytnacku(int x, int y) {
+        Turtle novaKorytnacka = new Turtle();
+        this.add(novaKorytnacka);
+        novaKorytnacka.setPosition(x, y);
+
+        Turtle[] noveKorytnacky = new Turtle[this.korytnacky.length + 1];
+        System.arraycopy(this.korytnacky, 0, noveKorytnacky, 0, this.korytnacky.length);
+        noveKorytnacky[noveKorytnacky.length-1] = novaKorytnacka;
+
+        this.korytnacky = noveKorytnacky;
+    }
+
+    @Override
+    protected void onMouseClicked(int x, int y, MouseEvent detail) {
+        if (!(detail.isAltDown() || detail.isControlDown() || detail.isShiftDown())) {
+            this.pridajKorytnacku(x, y);
+        }
+    }
+}
